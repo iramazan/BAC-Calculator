c739e07399357ed396b02c298f3c0b4f225e2d20
Ian Ramazan <iramazan96@gmail.com>
2018-11-29 21:52:46 -0500

    Add menu to top navbar.

--- app/src/main/java/csc420/baccalculator/MainActivity.java
+++ app/src/main/java/csc420/baccalculator/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,4 +25,37 @@ public class MainActivity extends AppCompatActivity {
         ft.replace(R.id.frag_holder, hf);
         ft.commit();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_overflow, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                // TODO: Switch to home fragment
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                HomeFragment hf = new HomeFragment();
+                ft.replace(R.id.frag_holder, hf);
+                ft.commit();
+                return true;
+            case R.id.menu_profile:
+                // TODO: Switch to profile fragment
+                return true;
+            case R.id.menu_contacts:
+                // TODO: Switch to contact fragment
+                return true;
+            case R.id.menu_settings:
+                // TODO: Switch to settings fragment
+                return true;
+            default:
+                // Not a recognized id
+                return false;
+        }
+    }
+
 }
